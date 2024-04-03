8a5953a319ad20e2cd52970abe2e4aa43f92e4f7
chryzcode <alabaolanrewaju13@gmail.com>
2024-04-03 15:34:48 +0100

    push

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,8 @@
 import Navbar from "./components/Navbar";
 import Hero from "./components/Hero";
 import HomeCards from "./components/HomeCards";
-import  JobListings  from "./components/JobListings";
+import JobListings from "./components/JobListings";
+import ViewAllJobs from "./components/ViewAllJobs";
 
 const App = () => {
   return (
@@ -9,13 +10,8 @@ const App = () => {
       <Navbar />
       <Hero />
       <HomeCards />
-      <JobListings/>
-
-      <section className="m-auto max-w-lg my-10 px-6">
-        <a href="jobs.html" className="block bg-black text-white text-center py-4 px-6 rounded-xl hover:bg-gray-700">
-          View All Jobs
-        </a>
-      </section>
+      <JobListings />
+      <ViewAllJobs />
     </>
   );
 };
