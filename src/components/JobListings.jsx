88307c7c00bd1606496f80820ccd30c0be35525a
chryzcode <alabaolanrewaju13@gmail.com>
2024-04-05 02:51:27 +0100

    push

--- src/components/JobListings.jsx
+++ src/components/JobListings.jsx
@@ -2,8 +2,24 @@ import { useState, useEffect } from "react";
 import JobListing from "./JobListing";
 
 const JobListings = ({ isHome = false }) => {
-  const [jobs, setJobs] = useEffect([]);
-  const [loading, setLoading] = useEffect(false);
+  const [jobs, setJobs] = useState([]);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    const fetchJobs = async () => {
+      try {
+        const res = await fetch("http://localhost:8000/jobs");
+        const data = await res.json();
+        setJobs(data);
+      } catch (error) {
+        console.log("Error fetching data", error);
+      } finally {
+        setLoading(false);
+      }
+    };
+
+    fetchJobs();
+  }, []);
 
   return (
     <section className="bg-blue-50 px-4 py-10">
@@ -12,9 +28,15 @@ const JobListings = ({ isHome = false }) => {
           {isHome ? "Recent Jobs" : " Browse Jobs"}
         </h2>
         <div className="grid grid-cols-1 md:grid-cols-3 gap-6">
-          {jobListings.map(job => (
-            <JobListing key={job.id} job={job} />
-          ))}
+          {loading ? (
+            <h2>loading...</h2>
+          ) : (
+            <>
+              {jobs.map(job => (
+                <JobListing key={job.id} job={job} />
+              ))}
+            </>
+          )}
         </div>
       </div>
     </section>
